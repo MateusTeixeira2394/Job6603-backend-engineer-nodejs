250b0ba3a8c5764ec8f5968cce243bcdcdc89a1d
Mateus Teixeira <mateus.teixeira@gx2.com.br>
2024-12-23 16:18:59 -0300

    Test submition

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+
+interface ITask {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+let tasks: ITask[] = [];
+
+const app = express();
+app.use(express.json())
+
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+app.post('/tasks', (req, res) => {
+  const task = req.body;
+  tasks.push(task);
+  res.json(task);
+})
+
+app.put('/tasks/:id', (req, res) => {
+
+  const id = req.params.id;
+  let body = req.body;
+  const index = tasks.findIndex(t => t.id === id);
+
+  if(index < 0){
+    res.json({message: 'task not found'})
+  }
+
+  tasks[index].id = body.id;
+  tasks[index].title = body.title;
+  tasks[index].completed = body.completed;
+
+  res.json(body);
+
+})
+
+app.delete('/tasks/:id', (req, res)=>{
+  const id = req.params.id;
+  tasks = tasks.filter(t => t.id !== id);
+  res.json({message: 'task has deleted successfully'})
+})
+
+const port = parseInt(process.env.PORT || '3000');
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
